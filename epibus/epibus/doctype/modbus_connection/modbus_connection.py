36e7d9f960ba4154f09fb27f8a7be814b5309d5f
Geordie Everitt <geveritt@appliedrelevance.com>
2022-10-20 22:23:28 +0000

    Trivial return message.

--- epibus/epibus/doctype/modbus_connection/modbus_connection.py
+++ epibus/epibus/doctype/modbus_connection/modbus_connection.py
@@ -11,3 +11,4 @@ class ModbusConnection(Document):
     @frappe.whitelist()
     def test_connection(self, host, port):
         client = ModbusTcpClient(host, port)
+        return "Connection successful" if client.connect() else "Connection failed"
