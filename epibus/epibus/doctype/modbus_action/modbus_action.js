59b5f23806bd2e7646f7a96b3badf1b4db0c7271
Geordie Everitt <geveritt@appliedrelevance.com>
2022-10-24 23:56:01 +0000

    Added Modbus Action doctype

---
+++ epibus/epibus/doctype/modbus_action/modbus_action.js
@@ -0,0 +1,8 @@
+// Copyright (c) 2022, Applied Relevance and contributors
+// For license information, please see license.txt
+
+frappe.ui.form.on('Modbus Action', {
+	// refresh: function(frm) {
+
+	// }
+});
--- epibus/epibus/doctype/modbus_connection/modbus_connection.js
+++ epibus/epibus/doctype/modbus_connection/modbus_connection.js
@@ -2,8 +2,8 @@
 // For license information, please see license.txt
 
 frappe.ui.form.on('Modbus Connection', {
-	refresh: function(frm) {
-		frm.add_custom_button(__('Test Connection'), function() {
+	refresh: function (frm) {
+		frm.add_custom_button(__('Test Connection'), function () {
 			frm.call({
 				doc: frm.doc,
 				method: 'test_connection',
@@ -11,12 +11,52 @@ frappe.ui.form.on('Modbus Connection', {
 					"host": frm.doc.host,
 					"port": frm.doc.port,
 				},
-				callback: function(r) {
+				callback: function (r) {
 					if (r.message) {
 						frappe.msgprint(r.message);
 					}
 				}
 			});
 		});
+	}
+});
+
+const portPrefixMap = {
+	"Digital Output Coil": "%QX",
+	"Digital Output Slave Coil": "%QX",
+	"Digital Input Contact": "%IX",
+	"Digital Input Slave Contact": "%IX",
+	"Analog Input Register": "%IW",
+	"Memory Register (16 bit)": "%MW",
+	"Memory Register (32 bit)": "%MD",
+	"Memory Register (64 bit)": "%ML",
+}
+
+frappe.ui.form.on("Modbus Location", {
+	location_type: function (frm, cdt, cdn) {
+		if (!frappe.get_doc(cdt, cdn).location_type) return;
+		// frappe.msgprint("Form frm is " + JSON.stringify(frm));
+		frappe.msgprint("Location Type Changed to " + frappe.get_doc(cdt, cdn).location_type);
+		// frappe.call({
+		// 	method: "erpnext.projects.doctype.timesheet.timesheet.get_activity_cost",
+		// 	args: {
+		// 		employee: frm.doc.employee,
+		// 		activity_type: frm.selected_doc.activity_type,
+		// 		currency: frm.doc.currency
+		// 	},
+		// 	callback: function (r) {
+		// 		if (r.message) {
+		// 			frappe.model.set_value(cdt, cdn, "billing_rate", r.message["billing_rate"]);
+		// 			frappe.model.set_value(cdt, cdn, "costing_rate", r.message["costing_rate"]);
+		// 			calculate_billing_costing_amount(frm, cdt, cdn);
+		// 		}
+		// 	}
+		// });
+	},
+	force_true: function (frm, cdt, cdn) {
+		frappe.msgprint("Force True");
+	},
+	force_false: function (frm, cdt, cdn) {
+		frappe.msgprint("Force False");
 	}
 });
