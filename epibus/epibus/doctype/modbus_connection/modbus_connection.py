d26f87e2ac0beaac9c0db4f6f70b32c39f27461d
Geordie Everitt <geveritt@appliedrelevance.com>
2022-10-24 23:55:29 +0000

    Added Modbus Action doctype

--- epibus/epibus/doctype/modbus_connection/modbus_connection.py
+++ epibus/epibus/doctype/modbus_connection/modbus_connection.py
@@ -11,4 +11,5 @@ class ModbusConnection(Document):
     @frappe.whitelist()
     def test_connection(self, host, port):
         client = ModbusTcpClient(host, port)
+
         return "Connection successful" if client.connect() else "Connection failed"
