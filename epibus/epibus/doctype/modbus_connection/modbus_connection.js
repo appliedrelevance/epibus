bb8baed7675f905e7159ba7dd8f6e954f989cedb
Geordie Everitt <geveritt@appliedrelevance.com>
2022-10-26 22:16:24 +0000

    Updated to Aarons code.

--- epibus/epibus/doctype/modbus_connection/modbus_connection.js
+++ epibus/epibus/doctype/modbus_connection/modbus_connection.js
@@ -22,14 +22,14 @@ frappe.ui.form.on('Modbus Connection', {
 });
 
 const portPrefixMap = {
-	"Digital Output Coil": "%QX",
-	"Digital Output Slave Coil": "%QX",
-	"Digital Input Contact": "%IX",
-	"Digital Input Slave Contact": "%IX",
-	"Analog Input Register": "%IW",
-	"Memory Register (16 bit)": "%MW",
-	"Memory Register (32 bit)": "%MD",
-	"Memory Register (64 bit)": "%ML",
+	"Digital Output Coil": "% QX",
+	"Digital Output Slave Coil": "% QX",
+	"Digital Input Contact": "% IX",
+	"Digital Input Slave Contact": "% IX",
+	"Analog Input Register": "% IW",
+	"Memory Register(16 bit)": "% MW",
+	"Memory Register(32 bit)": "% MD",
+	"Memory Register(64 bit)": "% ML",
 }
 
 frappe.ui.form.on("Modbus Location", {
@@ -37,21 +37,27 @@ frappe.ui.form.on("Modbus Location", {
 		if (!frappe.get_doc(cdt, cdn).location_type) return;
 		let locType = frappe.get_doc(cdt, cdn).location_type;
 		frappe.msgprint(`Location Type for ${frm.id} Changed to ${locType}`);
-		// frappe.call({
-		// 	method: "erpnext.projects.doctype.timesheet.timesheet.get_activity_cost",
-		// 	args: {
-		// 		employee: frm.doc.employee,
-		// 		activity_type: frm.selected_doc.activity_type,
-		// 		currency: frm.doc.currency
-		// 	},
-		// 	callback: function (r) {
-		// 		if (r.message) {
-		// 			frappe.model.set_value(cdt, cdn, "billing_rate", r.message["billing_rate"]);
-		// 			frappe.model.set_value(cdt, cdn, "costing_rate", r.message["costing_rate"]);
-		// 			calculate_billing_costing_amount(frm, cdt, cdn);
-		// 		}
-		// 	}
-		// });
+	},
+	plc_address: function (frm, cdt, cdn) {
+		if (!frappe.get_doc(cdt, cdn).plc_address) return;
+		let plc_address = frappe.get_doc(cdt, cdn).plc_address;
+		let locType = frappe.get_doc(cdt, cdn).location_type;
+		const prefix = portPrefixMap[locType];
+		let plcValues = plc_address.substring(prefix.length).split('.');
+		let calculatedModbus = (parseInt(plcValues[0]) * 8) + parseInt(plcValues[1]);
+		let location_type = calculatedModbus > 799 ? "Digital Output Slave Coil" : "Digital Output Coil";
+		frappe.model.set_value(cdt, cdn, "modbus_address", calculatedModbus);
+		frappe.model.set_value(cdt, cdn, "location_type", location_type);
+		frappe.msgprint(`PLC address changed to ${plc_address} and the value for the Modbus address should be ${calculatedModbus} and location Type should be ${location_type}`);
+	},
+	modbus_address: function (frm, cdt, cdn) {
+		if (!frappe.get_doc(cdt, cdn).modbus_address) return;
+		let modbus_address = frappe.get_doc(cdt, cdn).modbus_address;
+		let location_type = modbus_address > 799 ? "Digital Output Slave Coil" : "Digital Output Coil";
+		let calculatedPlc = `%QX${parseInt(modbus_address / 8)}.${modbus_address % 8}`;
+		frappe.model.set_value(cdt, cdn, "plc_address", calculatedPlc);
+		frappe.model.set_value(cdt, cdn, "location_type", location_type);
+		frappe.msgprint(`Modbbus address changed to ${modbus_address}, PLC address should be ${calculatedPlc} and location Type should be ${location_type}`);
 	},
 	force_true: function (frm, cdt, cdn) {
 		frappe.msgprint("Force True");
