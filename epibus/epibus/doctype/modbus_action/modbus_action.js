77721a52c12527e73f25efef395ccfad5faa2069
George Everitt <geveritt@appliedrelevance.com>
2023-10-02 17:38:12 -0400

    Version 2.0 Initial commit

--- epibus/epibus/doctype/modbus_action/modbus_action.js
+++ epibus/epibus/doctype/modbus_action/modbus_action.js
@@ -1,58 +1,58 @@
 // Copyright (c) 2022, Applied Relevance and contributors
 // For license information, please see license.txt
 
-
 const test_connection = (frm) => {
-	frappe.db.get_doc("Modbus Connection", frm.doc.connection)
-		.then(connection => {
-			let loc = connection.locations.find(location => location.name === frm.doc.location);
+	frappe.db
+		.get_doc("Modbus Connection", frm.doc.connection)
+		.then((connection) => {
+			let loc = connection.locations.find(
+				(location) => location.name === frm.doc.location
+			);
 			if (!loc) {
-				frappe.throw('Location not found');
+				frappe.throw("Location not found");
 			}
 			let locint = parseInt(loc.modbus_address);
 			let bitvalue = frm.doc.bit_value ? 1 : 0;
 			frm.call({
 				doc: frm.doc,
-				method: 'test_action',
+				method: "test_action",
 				args: {
-					"host": connection.host,
-					"port": connection.port,
-					"action": frm.doc.action,
-					"location": locint,
-					"bit_value": bitvalue
+					host: connection.host,
+					port: connection.port,
+					action: frm.doc.action,
+					location: locint,
+					bit_value: bitvalue,
 				},
 				callback: function (r) {
 					if (r.message) {
-						frappe.show_alert({ message: r.message, indicator: 'green' });
+						frappe.show_alert({ message: r.message, indicator: "green" });
 					}
-				}
+				},
 			});
 		});
-}
-
-const trigger_action = (frm) => {
-	frm.call({
-		doc: frm.doc,
-		method: 'trigger_action',
-		callback: function (r) {
-			if (r.message) {
-				frappe.show_alert({ message: r.message, indicator: 'green' });
-			}
-		}
-	})
-}
-
-frappe.ui.form.on('Modbus Action', {
-	refresh: function (frm) {
-		frm.add_custom_button(__('Trigger Action'), function () {
-			return trigger_action(frm);
+	const trigger_action = (frm) => {
+		frm.call({
+			doc: frm.doc,
+			method: "trigger_action",
+			callback: function (r) {
+				if (r.message) {
+					frappe.show_alert({ message: r.message, indicator: "green" });
+				}
+			},
 		});
-	},
-	location: function (frm) {
-		return trigger_action(frm);
-	},
-	bit_value: function (frm) {
-		return trigger_action(frm);
-	}
-});
+	};
 
+	frappe.ui.form.on("Modbus Action", {
+		refresh: function (frm) {
+			frm.add_custom_button(__("Trigger Action"), function () {
+				return trigger_action(frm);
+			});
+		},
+		location: function (frm) {
+			return trigger_action(frm);
+		},
+		bit_value: function (frm) {
+			return trigger_action(frm);
+		},
+	});
+};
--- epibus/epibus/doctype/modbus_location/modbus_location.py
+++ epibus/epibus/doctype/modbus_location/modbus_location.py
@@ -1,12 +1,31 @@
 # Copyright (c) 2022, Applied Relevance and contributors
 # For license information, please see license.txt
 
-# import frappe
+import frappe
 from frappe.model.document import Document
 
 
 class ModbusLocation(Document):
-    pass
+    @frappe.whitelist()
+    def toggle_location_pin(self):
+        # Fetch the corresponding Modbus Connection document
+        connection_doc = frappe.get_doc("Modbus Connection", self.connection_name)
+
+        # Determine the value to write based on the 'toggle' field
+        value_to_write = 1 if self.toggle else 0
+
+        # Write the value to the corresponding pin
+        write_status = connection_doc.write_location_value(self, value_to_write)
+
+        if write_status:
+            # Read the value back from the pin
+            current_value = connection_doc.read_location_value(self)
+
+            # Update the 'value' field with the read value
+            self.value = current_value
+            self.save()
+
+            return current_value
 
 
 # The Modbus addresses bind to PLC addresses based on the hierarchical address value, i.e. lower PLC addresses are
