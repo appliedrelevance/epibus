6b8d73b106255e4b8ec13a4180a4a3c8ec29e986
George Everitt <geveritt@appliedrelevance.com>
2023-12-11 16:16:32 -0500

    Updated form javascript for Modbus Action

--- epibus/epibus/doctype/modbus_action/modbus_action.js
+++ epibus/epibus/doctype/modbus_action/modbus_action.js
@@ -1,15 +1,13 @@
-// Copyright (c) 2022, Applied Relevance and contributors
-// For license information, please see license.txt
-
+// Function to test the connection
 const test_connection = (frm) => {
-	frappe.db
-		.get_doc("Modbus Connection", frm.doc.connection)
+	frappe.db.get_doc("Modbus Connection", frm.doc.connection)
 		.then((connection) => {
 			let loc = connection.locations.find(
 				(location) => location.name === frm.doc.location
 			);
 			if (!loc) {
-				frappe.throw("Location not found");
+				frappe.msgprint(__("Location not found"));
+				return;
 			}
 			let locint = parseInt(loc.modbus_address);
 			let bitvalue = frm.doc.bit_value ? 1 : 0;
@@ -30,29 +28,32 @@ const test_connection = (frm) => {
 				},
 			});
 		});
-	const trigger_action = (frm) => {
-		frm.call({
-			doc: frm.doc,
-			method: "trigger_action",
-			callback: function (r) {
-				if (r.message) {
-					frappe.show_alert({ message: r.message, indicator: "green" });
-				}
-			},
-		});
-	};
+};
 
-	frappe.ui.form.on("Modbus Action", {
-		refresh: function (frm) {
-			frm.add_custom_button(__("Trigger Action"), function () {
-				return trigger_action(frm);
-			});
-		},
-		location: function (frm) {
-			return trigger_action(frm);
-		},
-		bit_value: function (frm) {
-			return trigger_action(frm);
+// Function to trigger an action
+const trigger_action = (frm) => {
+	frm.call({
+		doc: frm.doc,
+		method: "trigger_action",
+		callback: function (r) {
+			if (r.message) {
+				frappe.show_alert({ message: r.message, indicator: "green" });
+			}
 		},
 	});
 };
+
+// Event handlers
+frappe.ui.form.on("Modbus Action", {
+	refresh: function (frm) {
+		frm.add_custom_button(__("Trigger Action"), function () {
+			return trigger_action(frm);
+		});
+	},
+	location: function (frm) {
+		return test_connection(frm); // Assuming you want to test the connection when location changes
+	},
+	bit_value: function (frm) {
+		return test_connection(frm); // Assuming you want to test the connection when bit_value changes
+	},
+});
