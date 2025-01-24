2180b7a21a75d1aee1f57a8800e45125c1f702ce
Geordie Everitt <geveritt@appliedrelevance.com>
2025-01-24 20:41:18 +0000

    Removed simulator code.

--- epibus/epibus/api/__init__.py
+++
@@ -1,2 +0,0 @@
-# Export simulator API methods
-from .simulator import get_simulators, start_simulator, stop_simulator, get_io_points
--- frontend/src/components/signals/index.tsx
+++
@@ -1,4 +0,0 @@
-export { default as SignalCard } from './signal-card';
-export { default as DigitalSignal } from './digital-signal';
-export { default as AnalogSignal } from './analog-signal';
-export { default as SignalGrid } from './signal-grid';
